8ba42efb6aff70a3b6b243d7ce65458baf8bf978
Victoria Cushing <victoriaccushing@gmail.com>
2019-06-30 02:55:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,34 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(nyc, new)
+  nyc.unshift(new)
+end
+
+#def using_unshift(bouroughs_in_nyc, new_neighborhood)
+#  bouroughs_in_nyc.unshift(new_neighborhood)
+#end
+
+def using_pop(continents)
+  continents.pop
+end
+
+def pop_with_args(dogs)
+  dogs.pop(2)
+end
+
+def using_shift(cities)
+  cities.shift
+end
+
+def shift_with_args(ice_cream)
+  ice_cream.shift(2)
+end
+  
+def using_concat(favorite1, favorite2)
+  favorite1.concat(favorite2)
+end
+
+def using_insert()
+  
